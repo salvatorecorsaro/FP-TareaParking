05d9e3539673eb006004f26205c83cb1d2ba3776
salvatorecorsaro <salvatore.corsaro@live.com>
2020-03-16 17:15:50 +0100

    Cleaning the code, missing input system

--- src/com/salvatorecorsaro/Car.java
+++ src/com/salvatorecorsaro/Car.java
@@ -2,68 +2,73 @@ package com.salvatorecorsaro;
 
 import java.util.Random;
 
-public class Car implements Runnable{
+public class Car implements Runnable {
 
 
-        private int carNumber;
+    private int carNumber;
 
-        public Car(int carNumber) {
-            this.carNumber = carNumber;
+    public Car(int carNumber) {
+        this.carNumber = carNumber;
+    }
+
+    @Override
+    public void run() {
+        try {
+            waitBeforeTryToPark();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        System.out.printf("Car #%d drive to parking \n", carNumber);
 
-        @Override
-        public void run() {
+        try {
+            Main.barrera.acquire();
+
+            int parkingNumber = -1;
+
+            synchronized (Main.parkingStatus) {
+                for (int i = 0; i < Main.parkingSize; i++) {
+                    if (!Main.parkingStatus[i]) {
+                        Main.parkingStatus[i] = true;
+                        parkingNumber = i;
+                        System.out.printf("Car #%d Parked on %d place.\n", carNumber, i);
+                        break;
+                    }
+                }
+            }
+
             try {
-                Thread.sleep(getRandomNumberInRange(1000, 2000));
+                waitBeforeExitParking();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.printf("Car #%d drive to parking \n", carNumber);
 
-            try {
-                Main.SEMAPHORE.acquire();
-
-                int parkingNumber = -1;
-
-                synchronized (Main.PARKING_PLACES) {
-                    for (int i = 0; i < Main.parkingSize; i++) {
-                        if (!Main.PARKING_PLACES[i]) {
-                            Main.PARKING_PLACES[i] = true;
-                            parkingNumber = i;
-                            System.out.printf("Car #%d Parked on %d place.\n", carNumber, i);
-                            break;
-                        }
-                    }
-                }
-
-                try {
-                    Thread.sleep(getRandomNumberInRange(1000, 2000));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                synchronized (Main.PARKING_PLACES) {
-                    //Free space for car
-                    Main.PARKING_PLACES[parkingNumber] = false;
-                }
-
-                Main.SEMAPHORE.release();
-                System.out.printf("Car #%d leave the parking.\n", carNumber);
-
-            } catch (InterruptedException e) {
-            }
-        }
-
-        private static int getRandomNumberInRange(int min, int max) {
-
-            if (min >= max) {
-                throw new IllegalArgumentException("max must be greater than min");
+            synchronized (Main.parkingStatus) {
+                Main.parkingStatus[parkingNumber] = false;
             }
 
-            Random r = new Random();
-            return r.nextInt((max - min) + 1) + min;
+            Main.barrera.release();
+            System.out.printf("Car #%d leave the parking.\n", carNumber);
+
+        } catch (InterruptedException e) {
+        }
+    }
+
+    private void waitBeforeExitParking() throws InterruptedException {
+        Thread.sleep(getRandomNumberInRange(1000, 2000));
+    }
+
+    private void waitBeforeTryToPark() throws InterruptedException {
+        Thread.sleep(getRandomNumberInRange(1000, 2000));
+    }
+
+    private int getRandomNumberInRange(int min, int max) {
+
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
         }
 
-
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
 
 }
--- src/com/salvatorecorsaro/Main.java
+++ src/com/salvatorecorsaro/Main.java
@@ -1,29 +1,34 @@
 package com.salvatorecorsaro;
 
 
-
-
-
 import java.util.concurrent.Semaphore;
 
 
 public class Main {
 
 
-     static int parkingSize = 2;
-    // Parking place occupied = true; free - false
-    static final boolean[] PARKING_PLACES = new boolean[parkingSize];
-    // Set fair to true that for method aсquire() garantie order
-   static final Semaphore SEMAPHORE = new Semaphore(parkingSize, true);
+    static int parkingSize;
+    static boolean[] parkingStatus;
+    static Semaphore barrera;
     private static int numberOfCars;
 
 
     public static void main(String[] args) throws InterruptedException {
+
+        parkingSize = 2;
         numberOfCars = 6;
-        for (int i = 0; i < numberOfCars; i++) {
+        settingTheParking();
+        parkingInAction();
+    }
+
+    private static void parkingInAction() {
+        for (int i = 0; i < numberOfCars; i++)
             new Thread(new Car(i)).start();
-//            Thread.sleep(500);
-        }
+    }
+
+    private static void settingTheParking() {
+        parkingStatus = new boolean[parkingSize];
+        barrera = new Semaphore(parkingSize, true);
     }
 
 
